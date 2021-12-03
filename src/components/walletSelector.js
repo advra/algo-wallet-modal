61136fb11c5c8aaf52cd24f166afb9b6d2e17676
Adrian D. Alonzo <aalonzo3@gatech.edu>
2021-12-02 21:12:29 -0800

    Updated MyModal component name

--- src/components/walletSelector.js
+++ src/components/walletSelector.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types';
 
-import { MyModal } from './mymodal'
+import { MyModal } from './MyModal';
 import { ProvideWallet, useWallet } from '../context/ProvideWallet';
 import { Button } from 'react-bootstrap';
 
